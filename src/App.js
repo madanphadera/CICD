c59fd2f8119d76ac680c81d5e76845433f002ce5
Biswash Giri <48654820+bi3wa3@users.noreply.github.com>
2026-03-18 08:05:54 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ export default function App() {
 									fontFamily: `Greycliff CF, ${theme.fontFamily}`,
 									fontWeight: 900,
 								})}>
-								Techaxis TODO lists Dockerized
+								Techaxis TODO lists morningclass
 							</Title>
 							<ActionIcon
 								color={'blue'}
